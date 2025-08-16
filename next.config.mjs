27fba3ccc4832b5ba252dc2f3fad0412e4b93444
Mariosimom <simoncik.mario@gmail.com>
2025-08-16 17:54:40 +0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "res.cloudinary.com" }
-    ]
-  }
+  reactStrictMode: true
 };
 export default nextConfig;
